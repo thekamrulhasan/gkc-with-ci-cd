e71c23fc02424358902c35eb07dfdb86d91f02d0
Mohammad Kamrul Hasan <55684962+thekamrulhasan@users.noreply.github.com>
2023-04-30 19:24:32 +0600

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const PORT = process.env.PORT || 9000
 http
   .createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello! Welcome GCP automation.')
+    res.end('Hello! Welcome GCP automation done by Kamrul Hasan.')
   })
   .listen(PORT, () => console.log('Listening on', PORT))
